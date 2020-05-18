8b5953899bbf7e98cad14b161c3a979a0c35d803
tommyrharper <tommyrharper@gmail.com>
2020-05-18 16:04:45 +0100

    Refactored to create new method create_statement

--- lib/account.rb
+++ lib/account.rb
@@ -3,7 +3,6 @@ class Account
   STATEMENT_HEADER = "date || credit || debit || balance\n"
 
   def initialize
-    @balance = 0
     @dates = []
     @deposits = []
     @statement_end = ""
@@ -11,24 +10,26 @@ class Account
 
   def deposit(amount, date)
     @deposits << amount
-    @balance += amount
     @dates << date
     @date = date
   end
 
   def print_statement
-    no_entries = @dates.length
-    balance_on_date = @balance
+    create_statement
+    puts STATEMENT_HEADER + @statement_end
+  end
+
+  private
+
+  def create_statement
+    total_entries = @dates.length
+    balance_on_date = @deposits.sum
 
     @dates.reverse.each_with_index { |date, index|
-      last_deposit = @deposits[no_entries-index-1]
-
+      last_deposit = @deposits[total_entries-index-1]
       @statement_end += "#{date} || #{last_deposit}.00 || || #{balance_on_date}.00\n"
-
-      balance_on_date -= @deposits[no_entries-index-1]
+      balance_on_date -= @deposits[total_entries-index-1]
     }
-
-    puts STATEMENT_HEADER + @statement_end
   end
 
 end
