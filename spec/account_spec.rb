3bdbfec1728646a96b467d46526959b22e8b9037
tommyrharper <tommyrharper@gmail.com>
2020-05-18 15:24:14 +0100

    Test for four deposits added

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -37,5 +37,18 @@ describe Account do
       "10/01/2012 || 1000.00 || || 1000.00\n").to_stdout
   end
 
+  it 'accepts a four deposits of 1000 and prints the statement' do
+    subject.deposit(1000, "10/01/2012")
+    subject.deposit(1000, "13/01/2012")
+    subject.deposit(1000, "15/01/2012")
+    subject.deposit(1000, "17/01/2012")
+    expect{ subject.print_statement }.to output(
+      "date || credit || debit || balance\n"+
+      "17/01/2012 || 1000.00 || || 4000.00\n"+
+      "15/01/2012 || 1000.00 || || 3000.00\n"+
+      "13/01/2012 || 1000.00 || || 2000.00\n"+
+      "10/01/2012 || 1000.00 || || 1000.00\n").to_stdout
+  end
+
 
 end
