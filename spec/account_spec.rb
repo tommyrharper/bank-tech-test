447a8985bce336cf5b84804c889affc74f5be8d3
tommyrharper <tommyrharper@gmail.com>
2020-05-18 14:40:51 +0100

    Test for depositing 2000 on a different date

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,10 +2,16 @@ require 'account'
 
 describe Account do
 
-  it 'accepts a deposit of 1000 with a date and prints the correct information' do
+  it 'accepts a deposit of 1000 with a date and prints the statement' do
     account = Account.new
     account.deposit(1000, "10/01/2012")
     expect{ account.print_statement }.to output("date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00").to_stdout
+  end
+
+  it 'accepts a deposit of 2000 on a new date and prints the statement' do
+    account = Account.new
+    account.deposit(2000, "13/01/2012")
+    expect{ account.print_statement }.to output("date || credit || debit || balance\n13/01/2012 || 2000.00 || || 2000.00").to_stdout
   end
 
 end
