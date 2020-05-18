a5448b14748853881f473d4623af9be1c3a82c27
tommyrharper <tommyrharper@gmail.com>
2020-05-18 14:43:11 +0100

    initialize function created with @balance added, deposit method updated

--- lib/account.rb
+++ lib/account.rb
@@ -1,10 +1,15 @@
 class Account
+  def initialize
+    @balance = 0
+  end
 
   def deposit(amount, date)
+    @balance = amount
+    @date = date
   end
 
   def print_statement
-   print "date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00"
+    print "date || credit || debit || balance\n#{@date} || #{@balance}.00 || || #{@balance}.00"
   end
 
 end
