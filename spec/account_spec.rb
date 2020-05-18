c13e3904463cd505272808a62705a2a577da9f1f
tommyrharper <tommyrharper@gmail.com>
2020-05-18 14:34:44 +0100

    Tests 1000 can be deposited and the statement printed

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,5 +1,11 @@
 require 'account'
 
 describe Account do
-  
+
+  it 'accepts a deposit of 1000 with a date and prints the correct information' do
+    account = Account.new
+    account.deposit(1000, "10/01/2012")
+    expect{ account.print_statement }.to output("date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00").to_stdout
+  end
+
 end
