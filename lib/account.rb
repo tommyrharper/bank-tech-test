2b50c2972c7a0f5413c94e055db4335817c502b7
tommyrharper <tommyrharper@gmail.com>
2020-05-18 20:44:35 +0100

    Code linted

--- lib/account.rb
+++ lib/account.rb
@@ -1,15 +1,14 @@
 class Account
-
-  STATEMENT_HEADER = "date || credit || debit || balance\n"
+  STATEMENT_HEADER = "date || credit || debit || balance\n".freeze
 
   def initialize
     @dates = []
     @deposits = []
-    @statement_end = ""
+    @statement = ''
   end
 
   def deposit(amount)
-    is_valid_number?(amount) 
+    valid_number?(amount)
     set_date
     @deposits.unshift(amount)
     @dates.unshift(@date)
@@ -17,7 +16,7 @@ class Account
   end
 
   def withdraw(amount)
-    is_valid_number?(amount)
+    valid_number?(amount)
     set_date
     @deposits.unshift(-amount)
     @dates.unshift(@date)
@@ -26,22 +25,27 @@ class Account
 
   def print_statement
     create_statement
-    puts STATEMENT_HEADER + @statement_end
+    puts STATEMENT_HEADER + @statement
   end
 
   private
 
   def create_statement
-    balance_on_date = @deposits.sum
+    balance = @deposits.sum
+    @dates.each_with_index do |date, index|
+      update_statment(date, balance, index)
+      balance -= @deposits[index]
+    end
+  end
 
-    @dates.each_with_index { |date, index|
-      if credit?(index)
-        @statement_end += "#{date} || #{'%.2f' % @deposits[index]} || || #{'%.2f' % balance_on_date}\n"
-      else
-        @statement_end += "#{date} || || #{'%.2f' % -@deposits[index]} || #{'%.2f' % balance_on_date}\n"
-      end
-      balance_on_date -= @deposits[index]
-    }
+  def update_statment(date, balance, index)
+    credit = '%.2f' % @deposits[index]
+    debit = '%.2f' % -@deposits[index]
+    @statement += if credit?(index)
+                    "#{date} || #{credit} || || #{'%.2f' % balance}\n"
+                  else
+                    "#{date} || || #{debit} || #{'%.2f' % balance}\n"
+                  end
   end
 
   def set_date
@@ -49,17 +53,17 @@ class Account
   end
 
   def credit?(index)
-    @deposits[index] > 0
+    @deposits[index].positive?
   end
 
-  def is_valid_number?(amount)
-    fail 'Must enter a number' unless amount.is_a? Numeric
-    fail 'Cannot enter more than two decimal points' if more_than_two_decimal_places?(amount)
+  def valid_number?(amount)
+    raise 'Must enter a number' unless amount.is_a? Numeric
+    raise 'Cannot enter more than two decimal points' if
+    more_than_two_decimal_places?(amount)
   end
 
   def more_than_two_decimal_places?(amount)
-    decimals = amount.to_s.split(".")[1]
-    decimals != nil && decimals.length > 2
+    decimals = amount.to_s.split('.')[1]
+    !decimals.nil? && decimals.length > 2
   end
-
 end
