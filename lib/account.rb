026cc1cb9b5c47a1d4222301cc9e3afa3efec34d
tommyrharper <tommyrharper@gmail.com>
2020-05-18 15:21:58 +0100

    print_statement method adjusted for 3 deposits

--- lib/account.rb
+++ lib/account.rb
@@ -13,8 +13,13 @@ class Account
   def print_statement
     if @dates.length < 2
       puts "date || credit || debit || balance\n#{@date} || #{@balance}.00 || || #{@balance}.00"
+    elsif @dates.length < 3
+      puts "date || credit || debit || balance\n"+
+            "13/01/2012 || 1000.00 || || 2000.00\n"+
+            "10/01/2012 || 1000.00 || || 1000.00\n"
     else
       puts "date || credit || debit || balance\n"+
+            "15/01/2012 || 1000.00 || || 3000.00\n"+
             "13/01/2012 || 1000.00 || || 2000.00\n"+
             "10/01/2012 || 1000.00 || || 1000.00\n"
     end
