88f39dc4298f78f26101409fd58cbee91fc9661c
tommyrharper <tommyrharper@gmail.com>
2020-05-19 20:49:33 +0100

    Test adapted now the Transaction class uses the balance

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -4,7 +4,7 @@ describe Transaction do
   date = Time.new(2012, 1, 10, 12)
   it 'initialises with a date, a type and an amount' do
     allow(Time).to receive(:now).and_return(date)
-    transaction = Transaction.new(1000, 'credit')
+    transaction = Transaction.new(1000, 'credit', 0)
     expect(transaction.date).to eq '10/01/2012'
     expect(transaction.amount).to eq 1000
     expect(transaction.type).to eq 'credit'
