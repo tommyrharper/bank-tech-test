6b7f374483d2d2f821e012c1222f774f3de91ab0
tommyrharper <tommyrharper@gmail.com>
2020-05-19 18:29:38 +0100

    date mocked in transaction_spec test

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,12 +1,12 @@
 require 'transaction'
 
 describe Transaction do
-  it 'initialises with a date, a credit, a debit and a balance' do
-    date = '10/01/2012'
-    transaction = Transaction.new(date, 1000, 0, 2000)
+  date = Time.new(2012, 1, 10, 12)
+  it 'initialises with a date, a credit and a debit' do
+    allow(Time).to receive(:now).and_return(date)
+    transaction = Transaction.new(1000, 0)
     expect(transaction.date).to eq '10/01/2012'
     expect(transaction.credit).to eq 1000
     expect(transaction.debit).to eq 0
-    expect(transaction.balance).to eq 2000
   end
 end
