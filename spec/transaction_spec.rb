1fef7bd014366f8db7274df55ebaeaaf80f40a5f
tommyrharper <tommyrharper@gmail.com>
2020-05-19 18:54:52 +0100

    Transaction class now initialises with a type and an amount

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,11 +2,11 @@ require 'transaction'
 
 describe Transaction do
   date = Time.new(2012, 1, 10, 12)
-  it 'initialises with a date, a credit and a debit' do
+  it 'initialises with a date, a type and an amount' do
     allow(Time).to receive(:now).and_return(date)
-    transaction = Transaction.new(1000, 0)
+    transaction = Transaction.new(1000, 'credit')
     expect(transaction.date).to eq '10/01/2012'
-    expect(transaction.credit).to eq 1000
-    expect(transaction.debit).to eq 0
+    expect(transaction.amount).to eq 1000
+    expect(transaction.type).to eq 'credit'
   end
 end
