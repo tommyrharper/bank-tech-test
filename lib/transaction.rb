50ab8696c66fa503155e5a331a6c461e94aeffcc
tommyrharper <tommyrharper@gmail.com>
2020-05-19 18:28:51 +0100

    valid_number? method added to Transaction class and over_two_decimal_places?

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,9 +1,24 @@
 class Transaction
-  attr_reader :date, :credit, :debit, :balance
-  def initialize(date, credit, debit, balance)
-    @date = date
+  attr_reader :date, :credit, :debit
+  def initialize(credit, debit)
+    valid_number?(credit)
+    valid_number?(debit)
+    @date = Time.now.strftime('%d/%m/%Y')
     @credit = credit
     @debit = debit
-    @balance = balance
+  end
+
+  private
+
+  def valid_number?(number)
+    raise 'Must enter a number' unless number.is_a? Numeric
+    raise 'Cannot enter more than two decimal points' if
+    over_two_decimal_places?(number)
+  end
+
+  def over_two_decimal_places?(number)
+    # This counts the number of decimal places
+    decimals = number.to_s.split('.')[1]
+    !decimals.nil? && decimals.length > 2
   end
 end
