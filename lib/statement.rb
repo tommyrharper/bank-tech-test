1de8a7ef3233ab708a1406a574096eb99ff3cb97
tommyrharper <tommyrharper@gmail.com>
2020-05-19 12:49:27 +0100

    Statement add method updated to deal with debits

--- lib/statement.rb
+++ lib/statement.rb
@@ -6,13 +6,11 @@ class Statement
 
   def add(transaction)
     date = transaction.date
-    credit = '%.2f' % transaction.credit
-    debit = transaction.debit
     balance = '%.2f' % transaction.balance
-
-    row = "#{date} || #{credit} || #{debit}|| #{balance}\n"
+    credit = transaction.credit.zero? ? '' : '%.2f' % transaction.credit + ' '
+    debit = transaction.debit.zero? ? '' : '%.2f' % transaction.debit + ' '
+    row = "#{date} || #{credit}|| #{debit}|| #{balance}\n"
     @statement_end.insert(0, row)
-
     STATEMENT_HEADER + @statement_end
   end
 end
