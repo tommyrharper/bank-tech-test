6f335a99a10f55539992bd52fcf8a4c6bb614073
tommyrharper <tommyrharper@gmail.com>
2020-05-19 18:55:42 +0100

    Test doubles changed to reflect changes in the transaction class

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,22 +2,18 @@ require 'statement'
 
 describe Statement do
   let(:transaction_double) do
-    double :Transaction, date: '10/01/2012', credit: 1000,
-                         debit: 0, balance: 1000
+    double :Transaction, date: '10/01/2012', amount: 1000, type: 'credit'
   end
   let(:transaction_double2) do
-    double :Transaction, date: '13/01/2012', credit: 0,
-                         debit: 500, balance: 500
+    double :Transaction, date: '13/01/2012', amount: 500, type: 'debit'
   end
   it 'creates an accurate statement with one deposit' do
     expect(subject.add(transaction_double, 1000)).to eq '' \
-      "date || credit || debit || balance\n" \
       "10/01/2012 || 1000.00 || || 1000.00\n"
   end
   it 'creates an accurate statement with one deposit, one withdrawal' do
     subject.add(transaction_double, 1000)
     expect(subject.add(transaction_double2, 500)).to eq '' \
-      "date || credit || debit || balance\n" \
       "13/01/2012 || || 500.00 || 500.00\n" \
       "10/01/2012 || 1000.00 || || 1000.00\n"
   end
