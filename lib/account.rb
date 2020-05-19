721c091dd6926825236625e34b9081bcd4015ac4
tommyrharper <tommyrharper@gmail.com>
2020-05-19 18:27:48 +0100

    private methods removed and transactions array removed account.rb

--- lib/account.rb
+++ lib/account.rb
@@ -6,39 +6,24 @@ class Account
 
   def initialize
     @balance = 0
-    # @transactions = []
     @statement = Statement.new
   end
 
   def deposit(amount)
-    valid_number?(amount)
+    transaction = Transaction.new(amount, 0)
     @balance += amount
-    @statement.add(Transaction.new(Time.now.strftime('%d/%m/%Y'), amount, 0, @balance))
+    @statement.add(transaction, @balance)
     "#{amount} deposited. Balance: #{@balance}"
   end
 
   def withdraw(amount)
-    valid_number?(amount)
+    transaction = Transaction.new(0, amount)
     @balance -= amount
-    @statement.add(Transaction.new(Time.now.strftime('%d/%m/%Y'), 0, amount, @balance))
+    @statement.add(transaction, @balance)
     "#{amount} withdrawn. Balance: #{@balance}"
   end
 
   def print_statement
     puts STATEMENT_HEADER + @statement.content
-  end
-
-  private
-
-  def valid_number?(number)
-    raise 'Must enter a number' unless number.is_a? Numeric
-    raise 'Cannot enter more than two decimal points' if
-    over_two_decimal_places?(number)
-  end
-
-  def over_two_decimal_places?(number)
-    # This counts the number of decimal places
-    decimals = number.to_s.split('.')[1]
-    !decimals.nil? && decimals.length > 2
   end
 end
