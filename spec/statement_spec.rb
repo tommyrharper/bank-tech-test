dd7af1488889d8126113993ce37e607f7f174a29
tommyrharper <tommyrharper@gmail.com>
2020-05-19 12:26:50 +0100

    Tests for adding two deposits in statement_spec

---
+++ spec/statement_spec.rb
@@ -0,0 +1,24 @@
+require 'statement'
+
+describe Statement do
+  let(:transaction_double) do
+    double :Transaction, date: '10/01/2012', credit: 1000,
+                         debit: '', balance: 1000
+  end
+  let(:transaction_double2) do
+    double :Transaction, date: '13/01/2012', credit: 1000,
+                         debit: '', balance: 2000
+  end
+  # it 'creates an accurate statement with one deposit' do
+  #   expect(subject.add(transaction_double)).to eq '' \
+  #     "date || credit || debit || balance\n" \
+  #     "10/01/2012 || 1000 || || 1000\n"
+  # end
+  it 'creates an accurate statement with two deposits' do
+    subject.add(transaction_double)
+    expect(subject.add(transaction_double2)).to eq '' \
+      "date || credit || debit || balance\n" \
+      "13/01/2012 || 1000.00 || || 2000.00\n" \
+      "10/01/2012 || 1000.00 || || 1000.00\n"
+  end
+end
