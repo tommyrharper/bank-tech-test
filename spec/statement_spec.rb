8a24d3879a24d3dc8a2655821d7043e35808c95b
tommyrharper <tommyrharper@gmail.com>
2020-05-19 12:28:30 +0100

    old test uncommented

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -9,11 +9,11 @@ describe Statement do
     double :Transaction, date: '13/01/2012', credit: 1000,
                          debit: '', balance: 2000
   end
-  # it 'creates an accurate statement with one deposit' do
-  #   expect(subject.add(transaction_double)).to eq '' \
-  #     "date || credit || debit || balance\n" \
-  #     "10/01/2012 || 1000 || || 1000\n"
-  # end
+  it 'creates an accurate statement with one deposit' do
+    expect(subject.add(transaction_double)).to eq '' \
+      "date || credit || debit || balance\n" \
+      "10/01/2012 || 1000.00 || || 1000.00\n"
+  end
   it 'creates an accurate statement with two deposits' do
     subject.add(transaction_double)
     expect(subject.add(transaction_double2)).to eq '' \
