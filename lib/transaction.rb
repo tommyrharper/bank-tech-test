15d4dc4aa2396325239cf68accb37dc24b1b3f1b
tommyrharper <tommyrharper@gmail.com>
2020-05-19 20:50:11 +0100

    Transaction now checks to see if user is going into the negative

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,23 +1,24 @@
 class Transaction
   attr_reader :date, :amount, :type
-  def initialize(amount, type)
-    valid_number?(amount)
+  def initialize(amount, type, balance)
     @date = Time.now.strftime('%d/%m/%Y')
     @type = type
     @amount = amount
+    valid_number?(balance)
   end
 
   private
 
-  def valid_number?(number)
-    raise 'Must enter a number' unless number.is_a? Numeric
+  def valid_number?(balance)
+    raise 'Must enter a number' unless @amount.is_a? Numeric
+    raise 'Reached account limit' if @type == 'debit' && @amount > balance
     raise 'Cannot enter more than two decimal points' if
-    over_two_decimal_places?(number)
+    over_two_decimal_places?
   end
 
-  def over_two_decimal_places?(number)
+  def over_two_decimal_places?
     # This counts the number of decimal places
-    decimals = number.to_s.split('.')[1]
+    decimals = @amount.to_s.split('.')[1]
     !decimals.nil? && decimals.length > 2
   end
 end
