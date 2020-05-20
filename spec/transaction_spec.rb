5e004109d79a30cba51d1efdee4f8ebb7a6e1a3c
tommyrharper <tommyrharper@gmail.com>
2020-05-20 12:33:01 +0100

    Edge cases added to Transaction tests

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -9,4 +9,19 @@ describe Transaction do
     expect(transaction.amount).to eq 1000
     expect(transaction.type).to eq 'credit'
   end
+
+  context 'edge cases' do
+    it 'does not accept a string as a parameter for amount' do
+      expect { Transaction.new('string', 'credit', 0) }
+        .to raise_error 'Must enter a number'
+    end
+    it 'does not accept a number with 3 d.p. as a parameter for amount' do
+      expect { Transaction.new(5.555, 'credit', 0) }
+        .to raise_error 'Cannot enter more than two decimal points'
+    end
+    it 'does not allow the user to withdraw into the negative' do
+      expect { Transaction.new(5.01, 'debit', 5) }
+        .to raise_error 'Reached account limit'
+    end
+  end
 end
