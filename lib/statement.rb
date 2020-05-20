b3bea26fb768f93cd4fee7ecd9c7eeb8cfbd863e
tommyrharper <tommyrharper@gmail.com>
2020-05-20 12:51:14 +0100

    minor change to add and convert to two d.p. method in statement.rb

--- lib/statement.rb
+++ lib/statement.rb
@@ -5,16 +5,16 @@ class Statement
   end
 
   def add(transaction, balance)
-    convert_to_two_decimal_places(transaction, balance)
+    convert_to_two_decimal_places(transaction.amount, balance)
     create_row(transaction.date, @balance, @amount, transaction.type)
     @content.insert(0, @row)
   end
 
   private
 
-  def convert_to_two_decimal_places(transaction, balance)
+  def convert_to_two_decimal_places(amount, balance)
     @balance = '%.2f' % balance
-    @amount = '%.2f' % transaction.amount
+    @amount = '%.2f' % amount
   end
 
   def create_row(date, balance, amount, type)
