1fe2d3fbc594eae224bd2d411da4c8b9c143fe89
tommyrharper <tommyrharper@gmail.com>
2020-05-21 14:55:17 +0100

    Refactor code to use a transactions array

--- lib/account.rb
+++ lib/account.rb
@@ -4,25 +4,27 @@ require_relative 'statement'
 class Account
   def initialize(statement = Statement.new, transaction_class = Transaction)
     @balance = 0
+    @transactions = []
     @statement = statement
     @transaction_class = transaction_class
   end
 
   def deposit(amount)
     transaction = @transaction_class.new(amount, 'credit', @balance)
+    @transactions << transaction
     @balance += amount
-    @statement.add(transaction, @balance)
     "#{amount} deposited. Balance: #{@balance}"
   end
 
   def withdraw(amount)
     transaction = @transaction_class.new(amount, 'debit', @balance)
+    @transactions << transaction
     @balance -= amount
-    @statement.add(transaction, @balance)
     "#{amount} withdrawn. Balance: #{@balance}"
   end
 
   def print_statement
+    @statement.add(@transactions)
     puts @statement.header + @statement.content
   end
 end
