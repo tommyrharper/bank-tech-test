fe730ae5fc6fd41238e23ab1a1b71e20439cea4d
tommyrharper <tommyrharper@gmail.com>
2020-05-21 12:58:37 +0100

    Add brackets to the transaction doubles in statement_spec.rb for readability

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,20 +2,37 @@ require 'statement'
 
 describe Statement do
   it 'creates an accurate statement with one deposit' do
-    transaction_double = double :Transaction, date: '10/01/2012', amount: 1000,
-                                              type: 'credit'
-    expect(subject.add(transaction_double, 1000)).to eq '' \
-      "10/01/2012 || 1000.00 || || 1000.00\n"
+    transaction_double = double(
+      :Transaction,
+      date: '10/01/2012',
+      amount: 1000,
+      type: 'credit'
+    )
+
+    expected_output = "10/01/2012 || 1000.00 || || 1000.00\n"
+
+    expect(subject.add(transaction_double, 1000)).to eq expected_output
   end
 
   it 'creates an accurate statement with one deposit, one withdrawal' do
-    transaction_double = double :Transaction, date: '10/01/2012', amount: 1000,
-                                              type: 'credit'
-    transaction_double2 = double :Transaction, date: '13/01/2012', amount: 500,
-                                               type: 'debit'
+    transaction_double = double(
+      :Transaction,
+      date: '10/01/2012',
+      amount: 1000,
+      type: 'credit'
+    )
+    transaction_double2 = double(
+      :Transaction,
+      date: '13/01/2012',
+      amount: 500,
+      type: 'debit'
+    )
+
     subject.add(transaction_double, 1000)
-    expect(subject.add(transaction_double2, 500)).to eq '' \
-      "13/01/2012 || || 500.00 || 500.00\n" \
-      "10/01/2012 || 1000.00 || || 1000.00\n"
+
+    expected_output = "13/01/2012 || || 500.00 || 500.00\n" \
+                      "10/01/2012 || 1000.00 || || 1000.00\n"
+
+    expect(subject.add(transaction_double2, 500)).to eq expected_output
   end
 end
