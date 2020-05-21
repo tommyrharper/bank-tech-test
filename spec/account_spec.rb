c232b86caaf22095ba7101866d348e11cbb7dc8d
tommyrharper <tommyrharper@gmail.com>
2020-05-21 19:36:10 +0100

    Remove meaningless testing from account_spec.rb

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -14,7 +14,7 @@ describe Account do
   end
 
   context 'withdrawls' do
-    it 'accepts a deposit and returns the amount and balance' do
+    it 'accepts a withdrawal and returns the amount and balance' do
       transaction_double = double(:Transaction, new: '')
       statement_double = double(:Statement, add: '')
       account = Account.new(statement_double, transaction_double)
@@ -25,45 +25,14 @@ describe Account do
     end
   end
 
-  context 'print_statement' do
-    it 'accepts a deposits of 10.55 and prints the statement' do
-      transaction_double = double(:Transaction, new: '')
-      statement_double = double(
-        :Statement,
-        add: '',
-        content: "10/01/2012 || 10.55 || || 10.55\n",
-        header: "date || credit || debit || balance\n"
-      )
-      account = Account.new(statement_double, transaction_double)
-
-      account.deposit(10.55)
-
-      expected_output = "date || credit || debit || balance\n" \
-                        "10/01/2012 || 10.55 || || 10.55\n"
-
-      expect { account.print_statement }.to output(expected_output).to_stdout
-    end
-
-    it 'accepts a deposits of 1000 and a withdrawl of 500
-      and prints the statement' do
-      transaction_double = double(:Transaction, new: '')
-      statement_double = double(
-        :Statement,
-        add: '',
-        content: "13/01/2012 || || 500.00 || 500.00\n" \
-                 "10/01/2012 || 1000.00 || || 1000.00\n",
-        header: "date || credit || debit || balance\n"
-      )
-      account = Account.new(statement_double, transaction_double)
-
-      account.deposit(1000)
-      account.withdraw(500)
-
-      expected_output = "date || credit || debit || balance\n" \
-                        "13/01/2012 || || 500.00 || 500.00\n" \
-                        "10/01/2012 || 1000.00 || || 1000.00\n"
-
-      expect { account.print_statement }.to output(expected_output).to_stdout
-    end
+  it 'print_statement returns nil' do
+    transaction_double = double(:Transaction, new: '')
+    statement_double = double(
+      :Statement,
+      add: '',
+      print_statement: nil
+    )
+    account = Account.new(statement_double, transaction_double)
+    expect(account.print_statement).to eq nil
   end
 end
