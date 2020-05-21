a97369a89dcd0606b05b8e07225901bfbb9d4536
tommyrharper <tommyrharper@gmail.com>
2020-05-21 11:48:05 +0100

    Changed date variable names and added spaces to feature_spec.rb

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,49 +1,53 @@
 require 'account'
 
 describe Account do
-  date1 = Time.new(2012, 1, 10, 12)
-  date2 = Time.new(2012, 1, 13, 12)
-  date3 = Time.new(2012, 1, 15, 12)
-  date4 = Time.new(2012, 1, 17, 12)
+  tenth_january = Time.new(2012, 1, 10, 12)
+  thirteenth_january = Time.new(2012, 1, 13, 12)
+  fifthteenth_january = Time.new(2012, 1, 15, 12)
+  seventeenth_january = Time.new(2012, 1, 17, 12)
 
   context 'deposits' do
     it 'accepts a deposits of 10.55 and prints the statement' do
-      allow(Time).to receive(:now).and_return(date1)
+      allow(Time).to receive(:now).and_return(tenth_january)
       subject.deposit(10.55)
       expect { subject.print_statement }.to output('' \
         "date || credit || debit || balance\n" \
         "10/01/2012 || 10.55 || || 10.55\n").to_stdout
     end
+
     it 'accepts a deposit of 1000 and prints the statement' do
-      allow(Time).to receive(:now).and_return(date1)
+      allow(Time).to receive(:now).and_return(tenth_january)
       subject.deposit(1000)
       expect { subject.print_statement }.to output('' \
         "date || credit || debit || balance\n" \
         "10/01/2012 || 1000.00 || || 1000.00\n").to_stdout
     end
+
     it 'accepts a deposit of 2000 on a new date and prints the statement' do
-      allow(Time).to receive(:now).and_return(date2)
+      allow(Time).to receive(:now).and_return(thirteenth_january)
       subject.deposit(2000)
       expect { subject.print_statement }.to output('' \
         "date || credit || debit || balance\n" \
         "13/01/2012 || 2000.00 || || 2000.00\n").to_stdout
     end
+
     it 'accepts two deposits of 1000 and prints the statement' do
-      allow(Time).to receive(:now).and_return(date1)
+      allow(Time).to receive(:now).and_return(tenth_january)
       subject.deposit(1000)
-      allow(Time).to receive(:now).and_return(date2)
+      allow(Time).to receive(:now).and_return(thirteenth_january)
       subject.deposit(1000)
       expect { subject.print_statement }.to output('' \
         "date || credit || debit || balance\n" \
         "13/01/2012 || 1000.00 || || 2000.00\n" \
         "10/01/2012 || 1000.00 || || 1000.00\n").to_stdout
     end
+
     it 'accepts three deposits of 1000 and prints the statement' do
-      allow(Time).to receive(:now).and_return(date1)
+      allow(Time).to receive(:now).and_return(tenth_january)
       subject.deposit(1000)
-      allow(Time).to receive(:now).and_return(date2)
+      allow(Time).to receive(:now).and_return(thirteenth_january)
       subject.deposit(1000)
-      allow(Time).to receive(:now).and_return(date3)
+      allow(Time).to receive(:now).and_return(fifthteenth_january)
       subject.deposit(1000)
       expect { subject.print_statement }.to output('' \
         "date || credit || debit || balance\n" \
@@ -51,14 +55,15 @@ describe Account do
         "13/01/2012 || 1000.00 || || 2000.00\n" \
         "10/01/2012 || 1000.00 || || 1000.00\n").to_stdout
     end
+
     it 'accepts four deposits of 1000 and prints the statement' do
-      allow(Time).to receive(:now).and_return(date1)
+      allow(Time).to receive(:now).and_return(tenth_january)
       subject.deposit(1000)
-      allow(Time).to receive(:now).and_return(date2)
+      allow(Time).to receive(:now).and_return(thirteenth_january)
       subject.deposit(1000)
-      allow(Time).to receive(:now).and_return(date3)
+      allow(Time).to receive(:now).and_return(fifthteenth_january)
       subject.deposit(1000)
-      allow(Time).to receive(:now).and_return(date4)
+      allow(Time).to receive(:now).and_return(seventeenth_january)
       subject.deposit(1000)
       expect { subject.print_statement }.to output('' \
         "date || credit || debit || balance\n" \
@@ -72,20 +77,21 @@ describe Account do
   context 'withdrawls' do
     it 'accepts a deposits of 1000 and a withdrawl of 500
       and prints the statement' do
-      allow(Time).to receive(:now).and_return(date1)
+      allow(Time).to receive(:now).and_return(tenth_january)
       subject.deposit(1000)
-      allow(Time).to receive(:now).and_return(date2)
+      allow(Time).to receive(:now).and_return(thirteenth_january)
       subject.withdraw(500)
       expect { subject.print_statement }.to output('' \
         "date || credit || debit || balance\n" \
         "13/01/2012 || || 500.00 || 500.00\n" \
         "10/01/2012 || 1000.00 || || 1000.00\n").to_stdout
     end
+
     it 'accepts a deposits of 10 and a withdrawl of 4.45
     and prints the statement' do
-      allow(Time).to receive(:now).and_return(date1)
+      allow(Time).to receive(:now).and_return(tenth_january)
       subject.deposit(10)
-      allow(Time).to receive(:now).and_return(date2)
+      allow(Time).to receive(:now).and_return(thirteenth_january)
       subject.withdraw(4.45)
       expect { subject.print_statement }.to output('' \
       "date || credit || debit || balance\n" \
@@ -98,13 +104,16 @@ describe Account do
     it 'does not accept a string as a parameter for deposit' do
       expect { subject.deposit('string') }.to raise_error 'Must enter a number'
     end
+
     it 'does not accept a string as a parameter for withdrawl' do
       expect { subject.withdraw('string') }.to raise_error 'Must enter a number'
     end
+
     it 'does not accept a number with 3 d.p. as a parameter for deposit' do
       expect { subject.deposit(5.555) }
         .to raise_error 'Cannot enter more than two decimal points'
     end
+
     it 'does not allow the user to withdraw into the negative' do
       expect { subject.withdraw(0.01) }
         .to raise_error 'Reached account limit'
