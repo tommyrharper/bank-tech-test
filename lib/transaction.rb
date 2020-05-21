3cb5b3c69384914c65ee3c8d5418f5716f1f6b8c
tommyrharper <tommyrharper@gmail.com>
2020-05-21 19:35:43 +0100

    Refactor initialize method to remove bad code smell in transaction.rb

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -5,21 +5,24 @@ class Transaction
     @date = Time.now.strftime('%d/%m/%Y')
     @type = type
     @amount = amount
-    valid_number?(balance)
-    @balance = calculate_balance(balance)
+    run_checks(balance)
   end
 
   private
+
+  def run_checks(balance)
+    valid_number?(balance)
+    @balance = calculate_balance(balance)
+  end
 
   def calculate_balance(balance)
     @type == 'credit' ? balance + amount : balance - amount
   end
 
   def valid_number?(balance)
-    raise 'Must enter a number' unless @amount.is_a? Numeric
+    raise 'Must enter a number' if !@amount.is_a? Numeric
     raise 'Reached account limit' if @type == 'debit' && @amount > balance
-    raise 'Cannot enter more than two decimal points' if
-    over_two_decimal_places?
+    raise 'Entered more than two decimal places' if over_two_decimal_places?
   end
 
   def over_two_decimal_places?
