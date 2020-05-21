c07470628ae85fd19b5632a7f0b597f77eeebf4c
tommyrharper <tommyrharper@gmail.com>
2020-05-21 11:48:23 +0100

    Removed let statements from statement_spec.rb

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,17 +1,18 @@
 require 'statement'
 
 describe Statement do
-  let(:transaction_double) do
-    double :Transaction, date: '10/01/2012', amount: 1000, type: 'credit'
-  end
-  let(:transaction_double2) do
-    double :Transaction, date: '13/01/2012', amount: 500, type: 'debit'
-  end
   it 'creates an accurate statement with one deposit' do
+    transaction_double = double :Transaction, date: '10/01/2012', amount: 1000,
+                                              type: 'credit'
     expect(subject.add(transaction_double, 1000)).to eq '' \
       "10/01/2012 || 1000.00 || || 1000.00\n"
   end
+
   it 'creates an accurate statement with one deposit, one withdrawal' do
+    transaction_double = double :Transaction, date: '10/01/2012', amount: 1000,
+                                              type: 'credit'
+    transaction_double2 = double :Transaction, date: '13/01/2012', amount: 500,
+                                               type: 'debit'
     subject.add(transaction_double, 1000)
     expect(subject.add(transaction_double2, 500)).to eq '' \
       "13/01/2012 || || 500.00 || 500.00\n" \
